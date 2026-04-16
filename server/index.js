11783c1f14f37fa8f8d685e3ed5452fa70c45568
xiarune <cecboo2@gmail.com>
2026-04-16 11:07:41 -0400

    Use environment PORT for Render deployment

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
